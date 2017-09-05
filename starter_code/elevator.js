bd518432deffc05faabd28d87ddbae1ec9f42783
lele <lele@crackberry.local>
2017-09-05 09:32:49 +0200

    first commit elevator

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,50 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = 'up';
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() { 
+   let intervalId = setInterval(() => this.update(), 1000);
+  }
+
+  stop() { 
+    clearInterval(intervalId);
+  }
+
+  update() { 
+    this.log();
+  }
+
+  _passengersEnter() { 
+    
+  }
+
+  _passengersLeave() {
+
+   }
+
+  floorUp() {
+    this.direction = 'up';
+    return (this.floor < this.MAXFLOOR) ? this.floor += 1 : this.stop();
+   }
+
+  floorDown() {
+    this.direction = 'down';
+    return (this.floor > 0) ? this.floor -= 1 : this.stop();
+   }
+
+  call() { 
+
+  }
+  
+  log() { 
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,15 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator = new Elevator();
+
+elevator.start();
+
+
+elevator.update();
+
+elevator.floorDown(); 
+
+
+
+
